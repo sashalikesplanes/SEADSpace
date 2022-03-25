968a2b5559214e97f7a5c529ce9a0dd6b78d4eeb
JvanZyl1 <J.R.vanZyl@student.tudelft.nl>
2022-03-25 19:34:50 +0100

    g

--- SolarArrayPower.py
+++ SolarArrayPower.py
@@ -30,7 +30,7 @@ solar_param_dict = {
 
 def get_path_efficiency():
     n_array_regulator = 0.95
-    n_pdcu_daylight = 0.96 * 0.95 * 0.95
+    n_pdcu_daylight = 0.96 * 0.95
     n_pdcu_eclipse = 0.96 * 0.95
     path_efficiency_daylight = n_array_regulator + n_pdcu_daylight
     path_efficiency_eclipse = n_pdcu_eclipse
