0168837b4564d0e36d171afd253b9ed02f4b0aab
Ugo Coveliers-Munzi <u.j.m.coveliers-munzi@student.tudelft.nl>
2022-03-25 12:35:54 +0100

    f

--- SolarArrays.py
+++ SolarArrays.py
@@ -2,9 +2,10 @@ import numpy as np
 
 h = 800000  # height of orbit in m
 R_mars = 3389.5e3  # m
-R_orbit = R_mars+(h)/2
+R_orbit = R_mars+h
 G = 6.673e-11
 M_mars = 6.39e23  # kg
 
 ##FIND ORBITAL PERIOD FOR CIRCULAR ORBIT##
+V=np.sqrt((G*M_mars)/R)
 T = np.sqrt((4 * np.pi**2 * R_orbit**3)/(G*M_mars))
