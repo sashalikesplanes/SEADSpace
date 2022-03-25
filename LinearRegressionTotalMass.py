f5b15855dca0490be2d2f40733162e28db435ee8
Ugo Coveliers-Munzi <u.j.m.coveliers-munzi@student.tudelft.nl>
2022-03-25 19:29:55 +0100

    f

--- LinearRegressionTotalMass.py
+++ LinearRegressionTotalMass.py
@@ -3,6 +3,9 @@ from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 
 #power-x  and mass-y
+power_our_sat=500
+
+
 
 m=np.array([1030.5,725,1120,2180,1337.2,2454,4332])
 power=np.array([656,750,460,2000,840,1200,2000]).reshape((-1,1))#reshape to column vector
@@ -14,4 +17,5 @@ intercept=model.intercept_
 x=np.linspace(400,2500,10000)
 y=gradient*x+intercept
 plt.plot(x,y)
+plt.axvline(x=power_our_sat,color='r')
 plt.show()
