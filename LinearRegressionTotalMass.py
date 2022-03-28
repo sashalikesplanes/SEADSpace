d70563a6589e17404f7c578a513df3df26b92aea
Ugo Coveliers-Munzi <u.j.m.coveliers-munzi@student.tudelft.nl>
2022-03-28 19:53:10 +0200

    changes

--- LinearRegressionTotalMass.py
+++ LinearRegressionTotalMass.py
@@ -8,9 +8,9 @@ import matplotlib.pyplot as plt
 
 def linregress():
 
-    power_our_sat=500
+    power_our_sat=1286
     mass=np.array([1030.5,725,1120,2180,1337.2,2454,4332])#launch mass
-    power=np.array([656,750,460,2000,840,1200,2000]).reshape((-1,1))#power outputted by solar arrays
+    power=np.array([980,750,460,2000,840,1200,2000]).reshape((-1,1))#power outputted by solar arrays
 
     model = LinearRegression().fit(power,mass)
     gradient=model.coef_
@@ -18,15 +18,23 @@ def linregress():
 
     x=np.linspace(400,2500,10000)
     y=gradient*x+intercept
+
+    m_launch = gradient*power_our_sat+intercept
     plt.plot(x,y)
+    plt.ylabel('Mass [kg]')
+    plt.xlabel('Power [Watts]')
     plt.axvline(x=power_our_sat,color='r')
     plt.show()
+    return m_launch
 
 #using mass fraction from appendix A p.896 of SMEAD (remote sensing sats.)
-m_launch=1000 #kg
-m_propellant=0.043*m_launch
-m_dry=0.957*m_launch
+m_launch=linregress()
+print('m_launch:',m_launch)
 
+eps_mass=39.22
+m_dry=eps_mass/0.243
+#m_propellant=0.043*m_launch
+m_dry_frac=0.418
 #percentages of mass wrt to dry mass
 payload_frac=0.346
 structure_frac=0.189
@@ -35,4 +43,17 @@ power_frac=0.243
 TTC_frac=0.034
 ADCS_frac=0.045
 propulsion_frac=0.061
+
+
+
+m_payload=payload_frac*m_dry 
+m_structure=structure_frac*m_dry
+m_thermal=thermal_frac*m_dry
+m_power = power_frac*m_dry
+m_TTC=TTC_frac*m_dry
+m_ADCS=ADCS_frac*m_dry
+m_prop=propulsion_frac*m_dry
+
+
+print(m_dry, m_payload, m_structure, m_thermal,'m_power:', m_power, m_TTC, m_ADCS, m_prop)
 
--- OrbitalCalculation.py
+++ OrbitalCalculation.py
@@ -15,9 +15,18 @@ def get_orbit_times(altitude):#ALTITUDE of orbit IN METERS
     i=np.arccos((-2*rho*R_orbit**(7/2))/(3*J2*R_mars**2 * np.sqrt(mu_mars)))#inclination angle for sun synchronous orbit at altitude of 800km
     
     V=np.sqrt((G*M_mars)/R_orbit)
+    print(V)
     T=2*np.pi*R_orbit/V
+    
     Theta = 2*np.arcsin(R_mars/R_orbit)#find angle of ecplise
     t_eclipse=(Theta/(2*np.pi))*T
     t_daylight=T-t_eclipse
+
+    print(T)
+    print(t_eclipse)
+    print(i*180/np.pi)
+    
     return t_daylight, t_eclipse, i*180/np.pi
+
+get_orbit_times(800000)
 
